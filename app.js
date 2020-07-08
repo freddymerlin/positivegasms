1c785d87e7ce4a69720ae392182619f51895322f
Freddy Merlin <freddymerlin258@gmail.com>
2020-07-08 13:02:59 +0000

    Added all the routes

--- app.js
+++ app.js
@@ -2,21 +2,31 @@ let express = require("express"),
 	app = express(),
 	bodyParser = require("body-parser"),
 	mongoose = require("mongoose"),
-	ejs = require("ejs");
+	ejs = require("ejs"),
+	moment = require('moment'),
+	methodOverride = require('method-override');
 
-mongoose.connect("mongodb://localhost:27017/positivegasms", { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/database_positivegasms", { useUnifiedTopology: true, useNewUrlParser: true });
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
 
 
 let blogSchema = new mongoose.Schema({
 	title: String,
+	description: String,
 	image: String,
+	image_subtitle:String, 
 	body: String,
 	category: String,
 	author: String,
-	created: {type: Date, default: Date.now}
+	author_image: String,
+	created: {
+        type: String, 
+        default: () => moment().format("dddd, MMMM Do YY")
+    }
 });
 
 let Blog = mongoose.model("Blog", blogSchema);
@@ -48,9 +58,28 @@ app.post("/blogs", function(req,res){
 
 app.get("/blogs/:id", function(req,res){
 	Blog.findById(req.params.id, function(err, foundBlog){
-		res.remder("show",{blog:foundBlog});
+		res.render("show",{blog:foundBlog});
 	});
 	
+});
+
+app.get("/blogs/:id/edit", function(req,res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		res.render("edit",{blog:foundBlog});
+	});
+	
+});
+
+app.put("/blogs/:id", function(req,res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		res.redirect("/blogs/"+req.params.id);
+	});
+});
+
+app.delete("/blogs/:id", function(req,res){
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		res.redirect("/blogs");
+	});
 });
 
 app.listen(3000,function(){
